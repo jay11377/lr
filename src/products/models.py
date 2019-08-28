6b21e2890467f358a2a232cb7d07b657f5613eca
Jeremy <jeremy@jprojet.fr>
2019-08-28 20:42:30 +0200

    Add Category Model

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+    def __str__(self):
+        return self.title

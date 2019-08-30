055f567c46dc462e09989c444c87af80d9e66a53
Jeremy <jeremy@jprojet.fr>
2019-08-30 21:15:30 +0200

    Route to get products from a category
    Use a url such as categories/3/products/ to get the products from category 3

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 from .models import Category, Product
 
 from . import serializers
@@ -14,6 +16,13 @@ class BaseViewSet (viewsets.ModelViewSet):
 class CategoriesViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CategorySerializer
     queryset = Category.objects.all()
+
+    @detail_route()
+    def products(self, request, pk=None):
+        category = self.get_object()  # retrieve an object by pk provided
+        products = Product.objects.filter(category=category).distinct()
+        products_json = serializers.ProductSerializer(products, many=True)
+        return Response(products_json.data)
 
 
 class ProductViewSet(viewsets.ModelViewSet):

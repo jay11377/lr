8a623ab838edf40d4a41423c0c2d874cfff1612b
Jeremy <jeremy@jprojet.fr>
2019-09-04 07:17:40 +0200

    Allow access to media folder

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('polls.urls')),
     path('api/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

1f31b0bcbe894ce489711ad75d557490d4b9fd68
Jeremy <jeremy.thebault.jp@gmail.com>
2019-10-10 22:12:51 +0200

    LR-90: Filter categories by current user

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,6 +3,18 @@ from django.contrib.admin import ModelAdmin
 from .models import Category, Product, Store
 
 
+class FilterUserAdmin(ModelAdmin):
+    # Force current user as author
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
+
+    # Filter view with author = current user
+    def get_queryset(self, request):
+        qs = super(FilterUserAdmin, self).get_queryset(request)
+        return qs.filter(author=request.user)
+
+
 # Register your models here.
 class InlineProduct(admin.TabularInline):
     model = Product
@@ -17,7 +29,7 @@ class StoreAdmin(ModelAdmin):
 admin.site.register(Store, StoreAdmin)
 
 
-class CategoryAdmin(ModelAdmin):
+class CategoryAdmin(FilterUserAdmin):
     list_display = ('title', 'author')
     list_filter = ('author',)
     inlines = [InlineProduct]

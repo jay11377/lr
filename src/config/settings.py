cf9955f3e5c74f4727964e796d7deac433acee6d
Jeremy <jeremy.thebault.jp@gmail.com>
2019-12-11 18:43:40 +0100

    LR-170: Install allauth

--- src/config/settings.py
+++ src/config/settings.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ] + COMMON + APPS
 
 if DEBUG:
@@ -153,3 +157,18 @@ STATIC_ROOT = (root - 1)('static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (root - 1)('media')
 THUMB_SIZE = (120, 80)
+
+#Auth
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_FORMS = {
+    'signup': 'products.forms.CustomSignupForm',
+}
